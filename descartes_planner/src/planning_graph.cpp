ebe7e53f15d9028f17ebb6e8c05dca5f7873fd1c
Brett Hemes <brhemes@mmm.com>
2016-09-16 14:29:04 +0000

    Removed RobotModel cloning in PlanningGraph::calculateJointSolutions
    After some investigation it seems that copies in each thread are not
    required

--- descartes_planner/src/planning_graph.cpp
+++ descartes_planner/src/planning_graph.cpp
@@ -186,9 +186,6 @@ bool PlanningGraph::calculateJointSolutions(const TrajectoryPtPtr* points, const
   bool success = true;
   #pragma omp parallel shared(success)
   {
-    //make private copy of robot model for each thread
-    descartes_core::RobotModelPtr model_copy = robot_model_->clone();
-
     #pragma omp for
     for (std::size_t i = 0; i < count; ++i)
     {
